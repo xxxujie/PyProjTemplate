fd8763da2b867318e7da3c37e3544fde528c6f8a
xxxujie-wsl <wsl-docker-lab@lightfall.com>
2024-08-03 19:10:10 +0000

    优化 main 结构

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# 程序入口
-# ! 不要在主函数入口之前添加任何执行代码
 import argparse
 import settings
 
@@ -7,20 +5,20 @@ from common.utils import configs, loggers
 
 
 def main():
-    # 定义一个解析器，用于命令行执行时解析附带的参数
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config_dir", type=str, default="")
-    args = parser.parse_args()
-    if args.config_dir != "":
-        settings.USER_CONFIG_DIRS.insert(0, args.config_dir)
-
-
-if __name__ == "__main__":
-    main()
-
     # 测试代码
     print(configs.sample_config.USER_ID)
     print(configs.sample_config.USER_FULL_NAME)
     print(configs.sample_config.TYPE)
     logger = loggers.get_logger()
     logger.info("run main")
+
+
+if __name__ == "__main__":
+    # 定义一个解析器，用于命令行执行时解析附带的参数
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_dir", type=str, default="")
+    args = parser.parse_args()
+    if args.config_dir != "":
+        settings.USER_CONFIG_DIRS.insert(0, args.config_dir)
+    logger = loggers.get_logger()
+    main()
