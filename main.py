aca00de83b32000dd9662727985228224d1f99f6
xxxujie <cxj604365090@163.com>
2024-08-09 11:27:04 +0800

    fix bugs

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     # 定义一个解析器，用于命令行执行时解析附带的参数
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_dir", type=str, default="")
-    parser.add_argument("--config_dir", type=str, default="")
     args = parser.parse_args()
     if args.config_dir != "":
         globalenv.USER_CONFIG_DIRS.insert(0, args.config_dir)
